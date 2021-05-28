ec5654d7bfeba411a61a513aaed7aba24325f767
curtis sloan <thezlayer32@hotmail.com>
2021-05-28 13:08:44 -0400

    Assingment 01 updated files

--- Not_Curtis_Blog/settings.py
+++ Not_Curtis_Blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wxkxaqm-r64)qwcs33ut7))s28%fh)vbo=cmr!x$3pm8y&=c5p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
